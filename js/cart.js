767f027091e280d9f535757e1f1c90c3a903cab1
Kiera <65538313@qq.com>
2017-10-28 14:02:42 +0800

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -17,7 +17,24 @@ new Vue({
         this.getData();
         //Q： 钩子的用法
     },
-
+    directives: {
+        numberOnly: {
+          bind: function(el) {
+            el.handler = function() {
+              var formatVal = /^\+?[1-9][0-9]*$/;
+              var val = el.value.toString();
+              if(!formatVal.test(val)){
+                var reg = new RegExp(val,'g');
+                el.value = el.value.replace(reg, 1)
+              }
+            }
+            el.addEventListener('input', el.handler)
+          },
+          unbind: function(el) {
+            el.removeEventListener('input', el.handler)
+          }
+        }
+      },
     computed: {
         allChecked: {
             get: function(){
